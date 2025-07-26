e36de653b5644a630a74f34b726cd0ad328b769e
Stanisław Mikołajczyk <mikolajczykstanislaw415@gmail.com>
2025-07-26 17:51:16 +0200

    Add HtmlWebpackPlugin to copy index.html to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,68 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
+    const isProduction = argv.mode === "production";
 
-  return {
-    entry: {
-      app: ['./assets/js/app.js', './assets/scss/app.scss']
-    },
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: isProduction ? 'app.[contenthash].js' : 'app.js',
-      assetModuleFilename: 'img/[name][ext]'
-    },
-    resolve: {
-      modules: [path.resolve(__dirname, 'node_modules'), 'node_modules']
-    },
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            'css-loader',
-            {
-              loader: 'sass-loader',
-              options: {
-                api: 'modern',
-                sassOptions: {
-                  includePaths: [path.resolve(__dirname, 'node_modules')]
-                }
-              }
-            }
-          ]
+    return {
+        entry: {
+            app: ["./assets/js/app.js", "./assets/scss/app.scss"],
         },
-        {
-          test: /\.(png|jpg|jpeg|gif|svg)$/,
-          type: 'asset/resource',
-          generator: {
-            filename: 'img/[name][ext]'
-          }
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: isProduction ? "app.[contenthash].js" : "app.js",
+            assetModuleFilename: "img/[name][ext]",
         },
-        {
-          test: /\.(woff|woff2|eot|ttf|otf)$/,
-          type: 'asset/resource',
-          generator: {
-            filename: 'fonts/[name][ext]'
-          }
-        }
-      ]
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-      new MiniCssExtractPlugin({
-        filename: isProduction ? 'app.[contenthash].css' : 'app.css'
-      })
-    ],
-    devtool: isProduction ? false : 'source-map',
-    watch: false
-  };
-}; 
+        resolve: {
+            modules: [path.resolve(__dirname, "node_modules"), "node_modules"],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                api: "modern",
+                                sassOptions: {
+                                    includePaths: [
+                                        path.resolve(__dirname, "node_modules"),
+                                    ],
+                                },
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.(png|jpg|jpeg|gif|svg)$/,
+                    type: "asset/resource",
+                    generator: {
+                        filename: "img/[name][ext]",
+                    },
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/,
+                    type: "asset/resource",
+                    generator: {
+                        filename: "fonts/[name][ext]",
+                    },
+                },
+            ],
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: isProduction ? "app.[contenthash].css" : "app.css",
+            }),
+            new HtmlWebpackPlugin({
+                template: "./index.html", // skopiuje do dist/
+            }),
+        ],
+        devtool: isProduction ? false : "source-map",
+        watch: false,
+    };
+};
